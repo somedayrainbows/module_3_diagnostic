c0c672a2d1d4b8f8241471b1e9cf4f8d32dde8bd
somedayrainbows <ebassity@gmail.com>
2017-05-17 11:05:14 -0600

    Adds to WIP fuel finder feature
    * Successfully pulling data needed form API via search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
 
   def index
-    @conn = Faraday.new("https://developer.nrel.gov")
-    response = conn.get '/api/alt-fuel-stations/v1/nearest.json', { location: params[:location], radius: params[:location], fuel_type: params[:fuel_type], limit: params[:limit], api_key: params[:api_key] }
+    conn = Faraday.new("https://developer.nrel.gov")
+    response = conn.get '/api/alt-fuel-stations/v1/nearest.json', { location: params[:location], radius: params[:radius], fuel_type: params[:fuel_type], limit: params[:limit], api_key: params[:api_key] }
+    byebug
   end
 
 end

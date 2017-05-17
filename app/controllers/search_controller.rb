4bf64b9eae2e3f171de7523770edafe8cc25b79b
somedayrainbows <ebassity@gmail.com>
2017-05-17 11:18:55 -0600

    Finishes fuel finder feature

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,8 @@ class SearchController < ApplicationController
   def index
     conn = Faraday.new("https://developer.nrel.gov")
     response = conn.get '/api/alt-fuel-stations/v1/nearest.json', { location: params[:location], radius: params[:radius], fuel_type: params[:fuel_type], limit: params[:limit], api_key: params[:api_key] }
-    byebug
+    @stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+    # byebug
   end
 
 end
